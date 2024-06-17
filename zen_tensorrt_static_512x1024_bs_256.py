b1f33c62bb016da52684124e8eb464fbe7e32d8b
Pieris Kalligeros <34139689+pieris98@users.noreply.github.com>
2024-06-18 00:55:36 +0300

    Add files via upload

---
+++ zen_tensorrt_static_512x1024_bs_256.py
@@ -0,0 +1,44 @@
+backend_config = dict(
+    common_config=dict(fp16_mode=False, max_workspace_size=21474836480),
+    model_inputs=[
+        dict(
+            input_shapes=dict(
+                input=dict(
+                    max_shape=[
+                        256,
+                        3,
+                        512,
+                        1024,
+                    ],
+                    min_shape=[
+                        256,
+                        3,
+                        512,
+                        1024,
+                    ],
+                    opt_shape=[
+                        256,
+                        3,
+                        512,
+                        1024,
+                    ]))),
+    ],
+    type='tensorrt')
+codebase_config = dict(task='Segmentation', type='mmseg', with_argmax=True)
+onnx_config = dict(
+    export_params=True,
+    input_names=[
+        'input',
+    ],
+    input_shape=[
+        1024,
+        512,
+    ],
+    keep_initializers_as_inputs=False,
+    opset_version=12,
+    optimize=True,
+    output_names=[
+        'output',
+    ],
+    save_file='end2end.onnx',
+    type='onnx')
